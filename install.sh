aa73056b1530ec59819f15e9aea81936fd76250b
Netuzb <62998831+Netuzb@users.noreply.github.com>
2022-04-15 14:49:53 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -204,7 +204,7 @@ runin ${SUDO_CMD}"python$PYVER" -m pip install -r requirements.txt --upgrade --u
 	errorin "Requirements failed!"
 	exit 4
 }
-endspin "Installation successful. Launching setup interface..."
+endspin "Userbot o'rnatildi! Endi: quyidagi Localhost toʻliq nusxalab, browserdan kiring..."
 rm -f ../ftg-install.log
 touch .setup_complete
 # shellcheck disable=SC2086,SC2015
