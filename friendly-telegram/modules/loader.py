8d692fba661d5a89c46c50f257715fdd1826fbb9
Netuzb <62998831+Netuzb@users.noreply.github.com>
2022-04-16 23:42:02 +0500

    Update loader.py

--- friendly-telegram/modules/loader.py
+++ friendly-telegram/modules/loader.py
@@ -175,7 +175,7 @@ class LoaderMod(loader.Module):
         super().__init__()
         self.config = loader.ModuleConfig(
             "MODULES_REPO",
-            "https://raw.githubusercontent.com/GeekTG/FTG-Modules/main/",
+            "https://raw.githubusercontent.com/Netuzb/FTG-Modules/main/",
             lambda m: self.strings("repo_config_doc", m),
         )
 
