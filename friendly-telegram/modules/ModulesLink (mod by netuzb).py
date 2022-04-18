8e83a3be0164029f6d7c1e8f67db38c2fab06fcf
Temur Erkinov <62998831+Netuzb@users.noreply.github.com>
2022-04-18 22:05:44 +0500

    Add files via upload

---
+++ friendly-telegram/modules/ModulesLink (mod by netuzb).py
@@ -0,0 +1,96 @@
+import io, inspect
+from .. import loader, utils
+
+
+@loader.tds
+class ModulesLinkMod(loader.Module):
+    """ Modullar [mod by #umodules]
+
+🇺🇲 • Retrieves already installed modules' links
+🇺🇿 • Oʻrnatilgan modullarning havolalarini osongina olish"""
+
+    strings = {"name": "Modullar"}
+
+    async def mlcmd(self, message):
+        """metod of sending file in EN language"""
+        args = utils.get_args_raw(message)
+        if not args:
+            return await utils.answer(message, "🚫 <b>No args</b>")
+
+        try:
+            f = " ".join(
+                [
+                    x.strings["name"]
+                    for x in self.allmodules.modules
+                    if args.lower() == x.strings["name"].lower()
+                ]
+            )
+            r = inspect.getmodule(
+                next(
+                    filter(
+                        lambda x: args.lower() == x.strings["name"].lower(),
+                        self.allmodules.modules,
+                    )
+                )
+            )
+
+            link = str(r).split("(")[1].split(")")[0]
+            if "http" not in link:
+                text = f"<b>🇺🇲 • {utils.escape_html(f)}</b>"
+            else:
+                text = f'🇺🇲 • <b><a href="{link}">Link</a> – {utils.escape_html(f)}</b> <a href="{link}"></a>'
+
+            out = io.BytesIO(r.__loader__.data)
+            out.name = f"{f}.py"
+            out.seek(0)
+
+            await message.respond(text, file=out)
+
+            if message.out:
+                await message.delete()
+        except:
+            await utils.answer(message, "😔 <b>Module not found</b>")
+
+# ----------------------------------------------------#
+# uzbek language mod by @netuzb
+# ----------------------------------------------------#
+
+    async def mluzcmd(self, message):
+        """modul faylini yuboruvchi metod UZ tilida"""
+        args = utils.get_args_raw(message)
+        if not args:
+            return await utils.answer(message, "🚫 <b>Arglar yo'q</b>")
+
+        try:
+            f = " ".join(
+                [
+                    x.strings["name"]
+                    for x in self.allmodules.modules
+                    if args.lower() == x.strings["name"].lower()
+                ]
+            )
+            r = inspect.getmodule(
+                next(
+                    filter(
+                        lambda x: args.lower() == x.strings["name"].lower(),
+                        self.allmodules.modules,
+                    )
+                )
+            )
+
+            link = str(r).split("(")[1].split(")")[0]
+            if "http" not in link:
+                text = f"<b>🇺🇿 • {utils.escape_html(f)}</b>"
+            else:
+                text = f'🇺🇿 • <b><a href="{link}">Havola</a> – {utils.escape_html(f)}</b> <a href="{link}"></a>'
+
+            out = io.BytesIO(r.__loader__.data)
+            out.name = f"{f}.py"
+            out.seek(0)
+
+            await message.respond(text, file=out)
+
+            if message.out:
+                await message.delete()
+        except:
+            await utils.answer(message, "😔 <b>Modul topilmadi</b>")
---
+++ friendly-telegram/modules/Qidiruv #umodules.py
@@ -0,0 +1,100 @@
+"""
+    ████░████░███░███░██░██░████░
+    ░██░░██░░░██░█░██░██░██░███░░
+    ░██░░████░██░░░██░█████░█████
+    ═════════════════════════════════════════
+    ████░████░░██░██░██░███░░██░█████░██░░░██
+    ██░░░███░░░████░░██░██░█░██░██░██░░██░██░
+    ████░█████░██░██░██░██░░███░█████░░░███░░
+    ═════════════════════════════════════════
+    Litsenziya: LLC © N.OA.ZL.QW (qaysi tupoy modul uchun litsenziya oladi? beradi?)
+    Taqdim qilingan sana: 12.03.2022 / 12:44
+    Taqdim qilingan manzil: https://telegram.me/umodules
+    ═════════════════════════════════════════
+    GeekTG yoki FTG oʻrnatish qoʻllanmasi: https://t.me/TGraphUz/1620
+    Kanallar roʻyhati: @UModules, @TGraphUz
+"""
+__version__ = (1, 0, 0)
+# meta developer: @umodules
+from .. import loader, utils 
+def register(cb):
+	cb(QidiruvMod()) 
+	
+class QidiruvMod(loader.Module):
+	"""Kontent qidirish moduli""" 
+	strings = {
+               "name": "Qidiruvchi", 
+               "umodules": "<b>[Qidiruv #umodules]</b> • ",
+               "easyapk": "<b>[EasyApk #kanali]</b> • ",
+               "uzbekona": "<b>[UZBEKONA #kanali]</b> • ",
+               "ftgchatuz": "<b>[Qidiruv #umodules]</b> • ",
+               "tgraphuz": "<b>[TGraphUz #kanali]</b> • ",
+               "yoq": "Buyruqdan keyin soʻz yozing deb qoʻyibman!?",
+               "topilmadi": "Hechnima topilmadi! Buyruqdan keyin faqat bitta soʻz yozishga urinib koʻring!"
+               }
+
+	async def tgraphcmd(self, message):
+		"""qidirish uchun soʻz yoki raqam"""
+		try:
+			title = utils.get_args_raw(message)
+			if not title:
+				await message.edit(self.strings("tgraphuz", message) + self.strings("yoq", message))
+			else:
+				umods = message.input_chat
+				await [i async for i in message.client.iter_messages("tgraphuz", search=title)][0].forward_to(umods)
+				await message.delete()
+		except:
+			await message.edit(self.strings("tgraphuz", message) + self.strings("topilmadi", message))
+	async def umodcmd(self, message):
+		"""qidirish uchun modul nomi yoki buyrugʻi"""
+		try:
+			title = utils.get_args_raw(message)
+			if not title:
+				await message.edit(self.strings("umodules", message) + self.strings("yoq", message))
+			else:
+				umods = message.input_chat
+				await [i async for i in message.client.iter_messages("umodules", search=title)][0].forward_to(umods)
+				await message.delete()
+		except:
+			await message.edit(self.strings("umodules", message) + self.strings("topilmadi", message))
+	async def umodgrcmd(self, message):
+		"""qidirish uchun soʻz (kirilchada faqat)"""
+		try:
+			title = utils.get_args_raw(message)
+			if not title:
+				await message.edit(self.strings("ftgchatuz", message) + self.strings("yoq", message))
+			else:
+				umods = message.input_chat
+				await [i async for i in message.client.iter_messages("ftgchatuz", search=title)][0].forward_to(umods)
+				await message.delete()
+		except:
+			await message.edit(self.strings("ftgchatuz", message) + self.strings("topilmadi", message))
+	async def uzbekonacmd(self, message):
+		"""qidirish uchun soʻz"""
+		try:
+			title = utils.get_args_raw(message)
+			if not title:
+				await message.edit(self.strings("uzbekona", message) + self.strings("yoq", message))
+			else:
+				umods = message.input_chat
+				await [i async for i in message.client.iter_messages("uzbekona", search=title)][0].forward_to(umods)
+				await message.delete()
+		except:
+			await message.edit(self.strings("uzbekona", message) + self.strings("topilmadi", message))
+	async def apkcmd(self, message):
+		"""dastur yoki oʻyin nomi"""
+		try:
+			title = utils.get_args_raw(message)
+			if not title:
+				await message.edit(self.strings("easyapk", message) + self.strings("yoq", message))
+			else:
+				umods = message.input_chat
+				await [i async for i in message.client.iter_messages("easyapk", search=title)][0].forward_to(umods)
+				await message.delete()
+		except:
+			await message.edit(self.strings("easyapk", message) + self.strings("topilmadi", message))
+
+# meta akakakakakakak jaakkakaak uchun quyidagi havolani Termux'da bajaring
+# meta Uzbekistan presents 
+# Barcha huquqlar himoyalangan!
+			
