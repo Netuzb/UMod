96418027045a70a9a19ce9cb88e2b813651e18c9
Temur Erkinov <62998831+Netuzb@users.noreply.github.com>
2022-05-03 10:09:32 +0500

    Update Sozlamalar.py

--- friendly-telegram/modules/Sozlamalar.py
+++ friendly-telegram/modules/Sozlamalar.py
@@ -37,7 +37,7 @@ class CoreMod(loader.Module):
         "packs_cleared": "<b>✅ Translations cleared</b>",
         "lang_set": "<b>✅ Language changed</b>",
         "db_cleared": "<b>📖 ✅ Barcha oʻzgarishlar tozalandi</b>",
-        "geek": "😎 <b>Balli! Sizda hozir UMod!\n\n📖 Versiya: 2.0.3\n📖 Soʻngi yangilanish sanasi: 03.05.2022</b>",
+        "geek": "🥷 <b>Malades! Sizda ''UMod!''\n├╴╴╴╴╴╴╴╴╴\n└ 👾 Versiya: <code>2.0.3</code>\n└ 👾 Soʻngi yangilanish sanasi: <code>03.05.2022</code></b>",
         "geek_beta": "🕶 <b>Congrats! You are UMod!</b>\n\n<b>UMod version: {}.{}.{}beta</b>\n<b>Branch: beta</b>\n\n<i>🔮 You're using the unstable branch (<b>beta</b>). You receive fresh but untested updates. Report any bugs to @ftgchatuz</i>",
         "geek_alpha": "🕶 <b>Congrats! You are UMod!</b>\n\n<b>UMod version: {}.{}.{}alpha</b>\n<b>Branch: alpha</b>\n\n<i>🔮 You're using <b><u>very</u></b> unstable branch (<b>alpha</b>). You receive fresh but untested updates. You <b><u>can't ask for help, only report bugs</u></b></i>",
     }
