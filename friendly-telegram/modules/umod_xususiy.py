0179fa3b4ca9ac99321a5318b2974c7195e3da79
Temur Erkinov <62998831+Netuzb@users.noreply.github.com>
2022-05-22 13:07:50 +0500

    Update umod_xususiy.py

--- friendly-telegram/modules/umod_xususiy.py
+++ friendly-telegram/modules/umod_xususiy.py
@@ -11,7 +11,7 @@ class GeekSettingsMod(loader.Module):
     """UMod sozlamalari"""
 
     strings = {
-        "name": "8.UModXususiy",
+        "name": "umod_xususiy",
         "watchers": "👀 <b>Kuzatuvchilar:</b>\n\n<b>{}</b>",
         "mod404": "🚫 <b>{} kuzatuvchisi topilmadi</b>",
         "already_disabled": "👀 <b>{} kuzatuvchisi allaqachon oʻchirib qoʻyilgan</b>",
