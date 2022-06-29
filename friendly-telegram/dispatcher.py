28d9689374659b0d33bc7e53b78ce464469684d6
temur erkinov <62998831+Netuzb@users.noreply.github.com>
2022-06-30 01:18:25 +0500

    Update dispatcher.py

--- friendly-telegram/dispatcher.py
+++ friendly-telegram/dispatcher.py
@@ -1,28 +1,9 @@
-#    Friendly Telegram (telegram userbot)
-#    Copyright (C) 2018-2022 The Authors
-
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-#    Modded by GeekTG Team
-
 import asyncio
 import collections
 import logging
 import re
 from telethon import types
 import traceback
-
 from . import utils, main, security, loader
 
 # Keys for layout switch
