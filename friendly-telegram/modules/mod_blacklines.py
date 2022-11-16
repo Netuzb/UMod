f622ed7c10ae2f79a183b80b3cf51a6091ea3b47
temur erkinov <62998831+Netuzb@users.noreply.github.com>
2022-11-16 11:30:49 +0500

    new module: Black Lines

---
+++ friendly-telegram/modules/mod_blacklines.py
@@ -0,0 +1,54 @@
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from .. import loader, utils
+
+
+def register(cb):
+	cb(BlackLinesMod())
+
+
+class BlackLinesMod(loader.Module):
+	"""Suratni @BlackLinesBot boti orqali noodatiy holatga aylantiradi"""
+
+	strings = {'name': 'Black Lines'}
+
+	def __init__(self):
+		self.name = self.strings['name']
+		self._me = None
+		self._ratelimit = []
+
+	async def client_ready(self, client, db):
+		self._db = db
+		self._client = client
+		self.me = await client.get_me()
+
+	async def linecmd(self, message):
+		"""[reply rasmga] sifat yuqori boʻlsa vaqti shunchali koʻp boʻladi """
+		
+		reply = await message.get_reply_message()
+		if not reply:
+			await message.edit("Suratga reply")
+			return
+		try:
+			photo = reply.media.photo
+		except:
+			await message.edit("Faqat suratga reply")
+			return
+		
+		args = utils.get_args_raw(message)
+				
+				
+		chat = '@BlackLinesBot'
+		await message.edit('@BlackLinesBot <code>Natijani kuting...</code>')
+		async with message.client.conversation(chat) as conv:
+			try:
+				response = conv.wait_event(events.NewMessage(incoming=True, from_users=1051644279))
+				await message.client.send_file(chat, photo, caption=args)
+				response = await response
+			except YouBlockedUserError:
+				await message.reply('<code>Botni blokdan chiqaring:</code> @BlackLinesBot')
+				return
+
+			await message.delete()
+			await message.client.send_file(message.to_id, response.media)
+			
